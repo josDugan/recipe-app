86c39fd25a1b5f891403e949c3c1e69588118e84
josDugan <josephdug@gmail.com>
2019-12-06 11:44:31 -0500

    Refactor number format exception handler into controller advice

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -73,17 +73,5 @@ public class RecipeController {
         return modelAndView;
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(NumberFormatException.class)
-    public ModelAndView handleNotFormatException(Exception e) {
-        log.error("Handling number format exception");
-        log.error(e.getMessage());
 
-        ModelAndView modelAndView = new ModelAndView();
-
-        modelAndView.setViewName("400error");
-        modelAndView.addObject("exception", e);
-
-        return modelAndView;
-    }
 }
